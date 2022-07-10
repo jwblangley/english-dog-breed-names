ac64ddfaae86f6b424d0320a87f222810d51a64e
James Langley <jwblangley@gmail.com>
2022-07-10 20:18:12 +0100

    Complete refactoring and type hinting

--- get_data.py
+++ get_data.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 from lxml import html
 from tqdm import tqdm
 
-from typing import Optional
+from typing import Generator, Iterator, Optional
 
 # Types
 Breed = namedtuple("Breed", ["name", "group"])
@@ -22,7 +22,21 @@ ENGLISH_NAME_X_PATH = '//*[@id="ContentPlaceHolder1_NomEnLabel"]'
 GROUP_X_PATH = '//*[@id="ContentPlaceHolder1_GroupeHyperLink"]'
 
 
-def print_results(breeds: list[Breed], outfile: Optional[str]):
+def _breeds_url_from_listing_page(page_content: str) -> list[tuple[str, str]]:
+    elems = html.fromstring(page_content).xpath(BREED_UL_LIST_PATH_X_PATH)
+    return [(breed.text_content(), breed.attrib["href"]) for breed in elems]
+
+
+def _breed_from_page(page_content: str) -> Breed:
+    tree = html.fromstring(page_content)
+    eng_name = tree.xpath(ENGLISH_NAME_X_PATH)[0].text_content()
+
+    group = tree.xpath(GROUP_X_PATH)[0].text_content()
+    group = group[group.index("-") + 2 :]
+    return Breed(eng_name.title(), group)
+
+
+def print_results(breeds: Iterator[Breed], outfile: Optional[str]) -> None:
     # Report results
     header = "Name, Group\n"
     format_gen = (f"{b.name}, {b.group}\n" for b in sorted(breeds))
@@ -38,6 +52,30 @@ def print_results(breeds: list[Breed], outfile: Optional[str]):
             print(f"Results written to {outfile}")
 
 
+def get_dog_listings(show_pbar: bool) -> Generator[Breed, None, None]:
+    alphabet_pbar = tqdm(ALPHABET)
+
+    for init in alphabet_pbar if show_pbar else ALPHABET:
+        if args.destination is not None:
+            alphabet_pbar.set_description(
+                f"Getting listings for dogs beginning with {init}"
+            )
+
+        page = requests.get(f"{BASE_URL}/en/nomenclature/races.aspx?init={init}")
+
+        breeds = _breeds_url_from_listing_page(page.content.decode("utf-8"))
+
+        breeds_pbar = tqdm(breeds)
+
+        for breed_name, breed_path in breeds_pbar if show_pbar else breeds:
+            if args.destination is not None:
+                breeds_pbar.set_description(f"Getting English name for {breed_name}")
+            subpage = requests.get(f"{BASE_URL}{breed_path}")
+
+            yield _breed_from_page(subpage.content.decode("utf-8"))
+        return
+
+
 if __name__ == "__main__":
     # Arguments
     parser = argparse.ArgumentParser()
@@ -49,35 +87,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    res = []
-
-    alphabet_pbar = ALPHABET if args.destination is None else tqdm(ALPHABET)
-
-    for init in alphabet_pbar:
-        if args.destination is not None:
-            alphabet_pbar.set_description(
-                f"Getting listings for dogs beginning with {init}"
-            )
-
-        page = requests.get(f"{BASE_URL}/en/nomenclature/races.aspx?init={init}")
-
-        breeds = html.fromstring(page.content).xpath(BREED_UL_LIST_PATH_X_PATH)
-
-        breeds_pbar = breeds if args.destination is None else tqdm(breeds)
-
-        for breed in breeds_pbar:
-            if args.destination is not None:
-                breeds_pbar.set_description(
-                    f"Getting English name for {breed.text_content()}"
-                )
-            subpage = requests.get(f"{BASE_URL}{breed.attrib['href']}")
-
-            tree = html.fromstring(subpage.content)
-            eng_name = tree.xpath(ENGLISH_NAME_X_PATH)[0].text_content()
-
-            group = tree.xpath(GROUP_X_PATH)[0].text_content()
-            group = group[group.index("-") + 2 :]
-
-            res.append(Breed(eng_name.title(), group))
-
-    print_results(res, args.desination)
+    print_results(get_dog_listings(args.destination is not None), args.destination)
